27c0d23d7ae604436891218e6631e65440e4f653
Volkan ATALAN <volkanatalan35@gmail.com>
2018-07-12 23:23:54 +0300

    v1.0

--- Example/src/main/java/volkanatalan/chartviewproject/fragments/PieChart.java
+++ Example/src/main/java/volkanatalan/chartviewproject/fragments/PieChart.java
@@ -1,6 +1,5 @@
 package volkanatalan.chartviewproject.fragments;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
